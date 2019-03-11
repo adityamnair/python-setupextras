44f4f6648eb23a3887820bfa4fe8a3f89194a206
Jonas Grimfelt <grimen@gmail.com>
2019-03-10 22:47:31 -0400

    init

---
+++ setupextras/__init__.py
@@ -0,0 +1,10 @@
+
+# =========================================
+#       IMPORTS / EXPORTS
+# --------------------------------------
+
+import rootpath
+
+rootpath.append()
+
+from setupextras.setupextras import *
