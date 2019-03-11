84101e51c9115040ea7be72b614cd056f8cfda12
Jonas Grimfelt <grimen@gmail.com>
2019-03-11 15:40:40 -0400

    version: v0.1.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ import setupextras
 # --------------------------------------
 
 name = 'setupextras'
-version = '0.1.4'
+version = '0.1.5'
 description = 'Additional common `setuptools` helpers - for Python.'
 keywords = [
     'setup',
